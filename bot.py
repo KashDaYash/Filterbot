5d0df8bfa926c03c39c52ec9a1b5860bd0941f38
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-09-05 18:13:12 +0530

    ok

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def run_in_executor(fn, *args, **kwargs):
 
   
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(start_bot())
+    asyncio.get_event_loop().run_until_complete(start())
     Bot().run()
     try:
         # Your bot's logic can go here
