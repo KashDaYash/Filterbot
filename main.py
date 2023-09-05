9c2475d3da6a2bc845adf90acf311cea2b150608
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-09-05 15:38:23 +0530

    ok

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from bot import Bot
 from pyrogram import idle
 
-Bot().run()
-idle()
+bot = Bot()
+
+async def main():
+    await bot.start()
+
+if __name__ == "__main__":
+    asyncio.run(main())
