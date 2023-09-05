04707ea66b71eceeb61f829f215e7093255e7872
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-09-05 18:10:55 +0530

    Kya Hoga ??

--- bot.py
+++ bot.py
@@ -14,42 +14,46 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-# Initialize the Pyrogram clients
-Client = Client(
-    "bot_session",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins={"root": "plugins"}
-)
-
 YaaraOP = Client(name="user_session", session_string=SESSION)
+# Initialize the Pyrogram clients
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            "bot_session",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins={"root": "plugins"}
+        )
 
-# Define the executor
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)  # Adjust max_workers as needed
 
-async def start_bot():
-    try:
-        await Client.start()
+        # Define the executor
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)  # Adjust max_workers as needed
+
+    async def start(self):
+      try:
+        await super().start()
         await YaaraOP.start()
         await YaaraOP.send_message("me", 'chtuiteg')  # Start the User client
         LOGGER.info("Bot Started ⚡")
-    except Exception as e:
+      except Exception as e:
         LOGGER.exception("Error while starting bot: %s", str(e))
 
-async def stop_bot():
-    try:
-        await Client.stop()
-        await YaaraOP.stop()
+    async def stop(self, *args):
+      try:
+        await super().stop()
+        await YaaraOP.stop()  # Stop the User client
         LOGGER.info("Bot Stopped")
-    except Exception as e:
+      except Exception as e:
         LOGGER.exception("Error while stopping bot: %s", str(e))
 
 def run_in_executor(fn, *args, **kwargs):
     return executor.submit(fn, *args, **kwargs)
 
+  
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(start_bot())
+    await Bot().run()
     try:
         # Your bot's logic can go here
         pass
