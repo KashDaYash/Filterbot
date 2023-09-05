ad5c916c79de1bdc45355f60b47e275418121b0c
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-09-05 19:12:22 +0530

    Okie

--- bot.py
+++ bot.py
@@ -48,16 +48,5 @@ class Bot(Client):
 
     def run_in_executor(self, fn, *args, **kwargs):
       return self.executor.submit(fn, *args, **kwargs)
-async def main():
-  await Bot().run()
-  idle()
+
   
-  
-if __name__ == "__main__":
-  asyncio.get_event_loop().run_until_complete(main())
-  try:
-    # Your bot's logic can go here
-    pass
-  except KeyboardInterrupt:
-    pass
-    
---
+++ main.py
@@ -0,0 +1,5 @@
+from bot import Bot
+
+
+Bot().run()
+idle()
