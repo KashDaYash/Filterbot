1686a1721558b539d9088be00b016261a211a740
yashattitude123@gmail.com <yashattitude123@gmail.com>
2024-01-07 19:12:49 +0530

    update __main__.py

--- yash/__main__.py
+++ yash/__main__.py
@@ -9,10 +9,10 @@ from yash.plugins import ALL_MODULES
 
 
 async def init():
+    await app.start() 
     for all_module in ALL_MODULES:
         importlib.import_module("yash.plugins." + all_module)
     LOGGER("yash.plugins").info("Successfully Imported Modules...")
-    await app.start()
     await yk.start()
     await idle()
     await app.stop()
