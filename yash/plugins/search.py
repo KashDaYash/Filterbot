f0822184b86a8530302d2265d7494238dc952ff4
yashattitude123@gmail.com <yashattitude123@gmail.com>
2024-01-08 19:19:22 +0530

    update search.py

--- yash/plugins/search.py
+++ yash/plugins/search.py
@@ -7,6 +7,7 @@ from config import *
 from pyrogram import *
 from pyrogram.errors import FloodWait
 import urllib.parse
+from yash import LOGGER 
 
 ignore_words = ["in", "and", "hindi", "movie", "tamil", "telugu", "dub", "hd", "man", "series", "full", "dubbed", "kannada", "season", "part", "all", "2022", "2021", "2023", "1", "2", "3", "4", "5", "6", "7" ,"8", "9", "0", "2020", "2019", "2018" , "2017", "2016", "2014", "all", "new", "2013", "()", "movies", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "1999", "1998", "1997", "1996", "1995", "-+:;!?*", "language", "480p", "720p", "1080p", "south", "Hollywood", "bollywood", "tollywood"]
 
@@ -36,6 +37,7 @@ async def search(bot, message):
     query = await clean_query(message.text)
     head = "<u>Here are the results 👇</u>\n\n"
     results = ""
+    LOGGER("umm").info("hua kuch")
     try:
         for channel in channels:
             async for msg in yk.search_messages(chat_id=channel, query=query):
